7c0298ae937fb78d2a39958934382f28842bbf7e
jae woo Jung <awfjol2008@gmail.com>
2026-01-20 20:51:38 +0900

    initialize project

---
+++ src/main/java/com/example/openticket/OpenTicketApplication.java
@@ -0,0 +1,13 @@
+package com.example.openticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenTicketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenTicketApplication.class, args);
+    }
+
+}
