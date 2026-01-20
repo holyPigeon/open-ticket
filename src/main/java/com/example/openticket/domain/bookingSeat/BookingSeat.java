ef1cb912f95e9b790e4eb0aa744afef21ca304ad
jae woo Jung <awfjol2008@gmail.com>
2026-01-21 00:23:18 +0900

    feat: Bookingseat 엔티티 작성

---
+++ src/main/java/com/example/openticket/domain/bookingSeat/BookingSeat.java
@@ -0,0 +1,42 @@
+package com.example.openticket.domain.bookingSeat;
+
+import com.example.openticket.domain.booking.Booking;
+import com.example.openticket.domain.seat.Seat;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class BookingSeat {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "booking_id", nullable = false)
+    private Booking booking;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "seat_id", nullable = false)
+    private Seat seat;
+
+    @Column(nullable = false)
+    private int price;
+
+    public BookingSeat(Booking booking, Seat seat) {
+        this.booking = booking;
+        this.seat = seat;
+        this.price = seat.getPrice();
+    }
+}
