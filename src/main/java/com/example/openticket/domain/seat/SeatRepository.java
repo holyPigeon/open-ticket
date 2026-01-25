c99cf85e055676d2f944ec555c5b194909dacc0d
jae woo Jung <awfjol2008@gmail.com>
2026-01-25 18:01:06 +0900

    feat(seat): 좌석 리포지토리 추가
    - `SeatRepository` 인터페이스를 추가하여 좌석 데이터에 대한 CRUD 기능 제공
    - `JpaRepository`를 상속받아 기본적인 데이터 접근 기능 구현

---
+++ src/main/java/com/example/openticket/domain/seat/SeatRepository.java
@@ -0,0 +1,7 @@
+package com.example.openticket.domain.seat;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SeatRepository extends JpaRepository<Seat, Long> {
+
+}
