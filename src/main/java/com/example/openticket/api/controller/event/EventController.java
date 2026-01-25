aec93b295c50efc83a6f49e71db941431bd98ba7
jae woo Jung <awfjol2008@gmail.com>
2026-01-25 22:10:29 +0900

    refactor(event): 이벤트 상세 조회 API 수정
    - `EventController`에서 요청 본문(`@RequestBody`) 제거
    - `@PathVariable`을 사용하여 이벤트 ID로 상세 조회하도록 변경

--- src/main/java/com/example/openticket/api/controller/event/EventController.java
+++ src/main/java/com/example/openticket/api/controller/event/EventController.java
@@ -4,7 +4,6 @@ import com.example.openticket.api.ApiResponse;
 import com.example.openticket.api.controller.event.dto.request.EventSearchRequest;
 import com.example.openticket.api.service.event.EventService;
 import com.example.openticket.api.service.event.dto.response.EventResponse;
-import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
@@ -13,7 +12,6 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -32,7 +30,6 @@ public class EventController {
 
     @GetMapping("/api/v1/events/{eventId}")
     public ApiResponse<EventResponse> getEventDetails(
-            @RequestBody @Valid EventSearchRequest request,
             @PathVariable Long eventId
     ) {
         return ApiResponse.ok(eventService.getEventDetails(eventId));
