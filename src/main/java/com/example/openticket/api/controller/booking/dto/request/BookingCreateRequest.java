e087ce7f282240cca5c479e603e9a7a1b38ef07c
jae woo Jung <awfjol2008@gmail.com>
2026-01-25 18:00:51 +0900

    feat(booking): 예약 생성 요청 DTO 추가
    - `BookingCreateRequest`와 `BookingCreateServiceRequest` 클래스 추가
    - 예약 생성 시 필요한 좌석 ID와 이벤트 ID를 포함
    - 서비스 요청으로 변환하는 메서드 구현

---
+++ src/main/java/com/example/openticket/api/controller/booking/dto/request/BookingCreateRequest.java
@@ -0,0 +1,14 @@
+package com.example.openticket.api.controller.booking.dto.request;
+
+import com.example.openticket.api.service.booking.dto.request.BookingCreateServiceRequest;
+import java.util.List;
+
+public record BookingCreateRequest(
+        List<Long> seatIds,
+        Long eventId
+) {
+    public BookingCreateServiceRequest toServiceRequest() {
+        return new BookingCreateServiceRequest(this.seatIds, this.eventId
+        );
+    }
+}
