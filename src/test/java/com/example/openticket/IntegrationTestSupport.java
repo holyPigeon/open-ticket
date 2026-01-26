a2750749e53d74aaca0b72c958323177986f7220
jae woo Jung <awfjol2008@gmail.com>
2026-01-26 22:57:46 +0900

    feat(test): Import 어노테이션을 통해 QueryDsl 설정 가져오도록 수정

--- src/test/java/com/example/openticket/IntegrationTestSupport.java
+++ src/test/java/com/example/openticket/IntegrationTestSupport.java
@@ -1,12 +1,15 @@
 package com.example.openticket;
 
+import com.example.openticket.config.QueryDslConfig;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
 @ActiveProfiles("test")
 @SpringBootTest
 @Transactional
+@Import(QueryDslConfig.class)
 public abstract class IntegrationTestSupport {
 
 }
