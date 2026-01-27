62bb8c302799a52f05c62a53f48fcde4a7a5b046
jae woo Jung <awfjol2008@gmail.com>
2026-01-27 23:22:12 +0900

    feat(global): JWT 토큰 생성 및 검증 기능 추가
    - `JwtProvider` 클래스 생성
    - 사용자 ID를 기반으로 JWT 토큰 생성 로직 구현
    - JWT 토큰의 ���효성 검증 기능 추가

---
+++ src/main/java/com/example/openticket/global/auth/JwtProvider.java
@@ -0,0 +1,60 @@
+package com.example.openticket.global.auth;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtProvider {
+
+    private final Key key;
+    private final long validityInMilliseconds;
+
+    public JwtProvider(
+            @Value("${security.jwt.secret-key:secret-key-must-be-very-long-for-security-reasons}") String secretKey,
+            @Value("${security.jwt.expire-length:3600000}") long validityInMilliseconds
+    ) {
+        this.key = Keys.hmacShaKeyFor(secretKey.getBytes());
+        this.validityInMilliseconds = validityInMilliseconds;
+    }
+
+    public String createToken(Long userId) {
+        Claims claims = Jwts.claims().setSubject(String.valueOf(userId));
+        Date now = new Date();
+        Date validity = new Date(now.getTime() + validityInMilliseconds);
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(now)
+                .setExpiration(validity)
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public Long getUserId(String token) {
+        return Long.parseLong(Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject());
+    }
+
+    // 토큰 유효성 검증
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
+
+            return true;
+        } catch (JwtException | IllegalArgumentException e) {
+            return false;
+        }
+    }
+}
