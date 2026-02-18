5842c1b9b9842004568d089d5f175cf4364cf3c1
jae woo Jung <awfjol2008@gmail.com>
2026-02-18 14:27:16 +0900

    feat(booking): 예약 생성 요청의 좌석 ID 유효성 검사 강화
    - `BookingCreateRequest`에서 좌석 ID 필드에 대해 비어있지 않음을 검증하는 `@NotEmpty` 어노테이션 추가
    - 유효하지 않은 요청에 대한 명확한 오류 메시지 제공

--- src/main/java/com/example/openticket/api/controller/booking/dto/request/BookingCreateRequest.java
+++ src/main/java/com/example/openticket/api/controller/booking/dto/request/BookingCreateRequest.java
@@ -1,11 +1,12 @@
 package com.example.openticket.api.controller.booking.dto.request;
 
 import com.example.openticket.api.service.booking.dto.request.BookingCreateServiceRequest;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import java.util.List;
 
 public record BookingCreateRequest(
-        @NotNull(message = "좌석 ID는 필수입니다.") List<Long> seatIds
+        @NotEmpty(message = "좌석 ID는 필수입니다.") List<Long> seatIds
 ) {
     public BookingCreateServiceRequest toServiceRequest() {
         return new BookingCreateServiceRequest(this.seatIds);
